f2fc65a46ec4371f75dc738aa5ec3d1c0c79537b
eivor9 <156003182+eivor9@users.noreply.github.com>
2024-04-09 09:50:03 -0400

    Add missing dollar sign to Progress.jsx

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -4,7 +4,7 @@ export default function Progress({targetAmount, donations}) {
   }, 0);
   return (
     <>
-      <h2>Raised <span className="secondary">{totalDonations}</span> of <span className="secondary">${targetAmount}</span></h2>
+      <h2>Raised <span className="secondary">${totalDonations}</span> of <span className="secondary">${targetAmount}</span></h2>
       <h3>You cound be donation <span className="secondary">#{donations.length + 1}!</span></h3>
     </>
   )
