8f56a4aafee41cb792ba54fc35c9b07f350590d7
kbodur <kbodur@pursuit.org>
2024-04-15 00:27:56 -0400

    add live site url

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,17 @@
-export default function Progress() {
-  return null;
+export default function Progress({ donations, targetAmount }) {
+  let total = 0;
+  for (const donation of donations) {
+    total += donation.amount;
+  }
+
+  return (
+    <>
+      <section className="progress">
+        <h2>
+          Raised <span className="secondary">${total}</span> of
+          <span className="secondary">${targetAmount}</span>
+        </h2>
+      </section>
+    </>
+  );
 }
