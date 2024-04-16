78ad5d09c38b7f9d35235445cf52aa1e5b26ad85
ViaticusRex <vdelacruz417@gmail.com>
2024-04-16 17:07:08 -0400

    completed

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,12 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${props.moneyRaised}</span> of
+        <span className="secondary"> ${props.totalAmount}</span>
+      </h2>
+    </section>
+  )
 }
+
+
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,15 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        <li><span>{props.donations[0].name} donated ${props.donations[0].amount}</span>{props.donations[0].caption}</li>
+        <li><span>{props.donations[1].name} donated ${props.donations[1].amount}</span>{props.donations[1].caption}</li>
+        <li><span>{props.donations[2].name} donated ${props.donations[2].amount}</span>{props.donations[2].caption}</li>
+        <li><span>{props.donations[3].name} donated ${props.donations[3].amount}</span>{props.donations[3].caption}</li>
+        <li><span>{props.donations[4].name} donated ${props.donations[4].amount}</span>{props.donations[4].caption}</li>
+        
+      </ul>
+    </section>
+  )
 }
