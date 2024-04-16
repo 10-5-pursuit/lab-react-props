7b6376e31d926e792f77489c3a11ad0ab465f5ef
sadya104 <sandrickson@pursuit.com>
2024-04-16 15:51:13 -0400

    update

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,15 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations, totalAmount}) {
+let value = 0
+for(let donation of donations){
+  value += donation.amount
+}
+
+  return (
+    <section className="progress">
+  <h2>
+    Raised <span className="secondary">{value}</span> of
+    <span className="secondary">{totalAmount}</span>
+  </h2>
+</section>
+  )
 }
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,18 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({ donations }) {
+
+donations = donations.map((donation) => {
+  const {amount, caption, name} = donation
+
+
+  return (
+      <li><span>{name} donated {amount}</span>{caption}</li>
+   )
+   })
+  return (
+<section>
+    <h2> Recent Donations</h2>
+    <ul>{donations}</ul>
+    </section>
+  )
 }
+
