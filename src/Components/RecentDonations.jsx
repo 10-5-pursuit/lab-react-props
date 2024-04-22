e2117ea2b6a1794a7e7c5947f10f9f37444c2761
Erimed33 <erikamedina@pursuit.org>
2024-04-22 19:07:28 -0400

    props-react

--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,23 @@
-export default function RecentDonations() {
-  return null;
+import React from 'react'
+import './recentdonations.css'
+
+const RecentDonations = ({donations}) => {
+  return (
+    <section>
+    <h2>RecentDonations</h2>
+
+    <ul>
+{donations.map((donation) => (
+  <li key={donation.id}>
+    <span>{ `${donation.name} donated $${donation.amount}`}</span><p>
+{donation.caption}</p>
+</li> 
+)
+)}
+    </ul>
+      </section>
+      
+  )
 }
+
+export default RecentDonations
